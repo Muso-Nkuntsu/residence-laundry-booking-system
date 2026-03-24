ef7bc9e0fa2f41494a9d2f3a60bca41e8e833ef2
snalo noni <230541844@mycput.ac.za>
2026-03-24 22:38:45 +0200

    feat: implement PaymentRepository with Singleton and HashSet

---
+++ src/main/java/za/ac/cput/repository/IPaymentRepository.java
@@ -0,0 +1,13 @@
+package za.ac.cput.repository;
+
+import za.ac.cput.domain.Payment;
+import java.util.Set;
+
+public interface IPaymentRepository extends IRepository<Payment, String> {
+    Set<Payment> getAll();
+
+    @Override
+    default Payment read(String s) {
+        return null;
+    }
+}
